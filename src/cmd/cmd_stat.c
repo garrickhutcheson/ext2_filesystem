453aeb3c949ea6200f36109f1dc8eff2da3f346d
ace cassidy <ac27182@gmail.com>
2019-04-07 15:48:12 -0700

    prototype stat

--- src/cmd/cmd_stat.c
+++ src/cmd/cmd_stat.c
@@ -1,6 +1,28 @@
 #include "cmd.h"
 
 bool do_stat(cmd *c) {
-  printf("command not yet implemented\n");
-  return 0;
+  path in_path;
+  minode *mip;
+  if (c->argc < 3) {
+    printf("stat requires: stat filename\n");
+    return false;
+  }
+  if (!parse_path(c->argv[1], &in_path) || !(mip = search_path(&in_path))) {
+    printf("bad path");
+    return false;
+  }
+  inode *i = &mip->inode;
+  printf("File: %s\n"
+         "Size: %u\t Blocks: %u\t Mode: %o\n"
+         "Device: %s\t Ino: %u\t Links: %u\t \n"
+         "Uid: %u\t Gid: %u\t \n"
+         "Access: %s\n"
+         "Modify: %s\n"
+         "Change: %s\n",
+         c->argv[1], i->i_size, i->i_blocks, i->i_mode,
+         mip->mount_entry->dev_path, mip->ino, i->i_links_count, i->i_uid,
+         i->i_gid, ctime((long *)&i->i_atime), ctime((long *)&i->i_mtime),
+         ctime((long *)&i->i_ctime));
+
+  return true;
 }
